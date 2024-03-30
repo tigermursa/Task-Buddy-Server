f9c65e23c095dd64ac25d8d1bb3ff5622b55d6f9
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-31 02:15:57 +0600

    "z validation file created"

---
+++ src/app/modules/tasks/tasks.validation.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const TaskValidationZodSchema = z.object({
+    title: z.string().min(3, { message: "Title must be at least 3 characters long" }),
+    description: z.string().min(10, { message: "Description must be at least 10 characters long" }),
+    category: z.enum(['personal', 'official', 'family']),
+    isImportant: z.boolean().default(false),
+    isDeleted: z.boolean().default(false),
+});
+
+export default TaskValidationZodSchema;
